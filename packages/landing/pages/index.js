c49e9fe54c4afc14c2f66109051eab7a991b7723
Olle Werme <olle.werme@gmail.com>
2022-01-11 21:04:46 +0100

    Bare minimum

--- packages/landing/pages/index.js
+++ packages/landing/pages/index.js
@@ -11,15 +11,58 @@ export default function Home() {
 
       <div
         style={{
-          minHeight: "100vh",
-          background: "black",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          color: "white",
+          fontSize: "min(15vh, 10.5vw)",
+          fontWeight: "800",
+          lineHeight: 1.25,
+          padding: "0.6em",
         }}
       >
-        <NewShadesLogo style={{ width: "92px", height: "auto" }} />
+        <NewShadesLogo
+          style={{
+            display: "block",
+            width: "0.6em",
+            height: "auto",
+            margin: "0 0 0.3em",
+          }}
+        />
+        <h1>NewShades DAO</h1>
+        <nav>
+          <ul>
+            {[
+              { href: "https://airtable.com/shrgbpYYVsuyEJxmW", label: "Join" },
+              { href: "https://twitter.com/newshadesDAO", label: "Twitter" },
+              { href: "http://discord.gg/gb4bbaT4", label: "Discord" },
+            ].map(({ label, href }) => (
+              <li key={href}>
+                <a href={href} target="_blank">
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
+
+      <style jsx>
+        {`
+          h1 {
+            font: inherit;
+          }
+          ul,
+          li {
+            margin: 0;
+            padding: 0;
+          }
+          nav li {
+            display: block;
+          }
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+        `}
+      </style>
     </>
   );
 }
