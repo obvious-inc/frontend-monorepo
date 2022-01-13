b0a68cb12295ba2cacd691b6b562c410f24a1451
Olle Werme <olle.werme@gmail.com>
2022-01-13 20:03:54 +0100

    Fix favicon

--- packages/landing/pages/index.js
+++ packages/landing/pages/index.js
@@ -6,7 +6,18 @@ export default function Home() {
     <>
       <Head>
         <title>NewShades DAO</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
 
       <div
