7c65e77c1867436678d07305927b20a2422d78f5
Olle Werme <olle.werme@gmail.com>
2022-01-19 14:58:46 +0100

    Copy favicons from landing

--- packages/desktop/webpack.config.js
+++ packages/desktop/webpack.config.js
@@ -1,17 +1,47 @@
+const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const API_SERVER = process.env.API_SERVER ?? "http://localhost:5001";
 
 module.exports = (_, argv) => {
+  const isProduction = argv.mode === "production";
+
+  const plugins = [
+    new HtmlWebpackPlugin({ template: "src/index.web.html.ejs" }),
+    new webpack.EnvironmentPlugin({
+      API_ENDPOINT: isProduction ? null : "/api",
+    }),
+  ];
+
+  if (isProduction)
+    plugins.push(
+      ...[
+        new CopyPlugin({
+          patterns: [
+            {
+              from: path.resolve(
+                __dirname,
+                "../landing/public/favicon-16x16.png"
+              ),
+            },
+            {
+              from: path.resolve(
+                __dirname,
+                "../landing/public/favicon-32x32.png"
+              ),
+            },
+          ],
+        }),
+      ]
+    );
+
   return {
     entry: "./src/web-entry.js",
     devServer: {
       proxy: {
-        "/api": {
-          target: API_SERVER,
-          pathRewrite: { "^/api": "" },
-        },
+        "/api": { target: API_SERVER, pathRewrite: { "^/api": "" } },
       },
     },
     module: {
@@ -30,11 +60,6 @@ module.exports = (_, argv) => {
         },
       ],
     },
-    plugins: [
-      new webpack.EnvironmentPlugin({
-        API_ENDPOINT: argv.mode === "production" ? null : "/api",
-      }),
-      new HtmlWebpackPlugin({ template: "src/index.web.html.ejs" }),
-    ],
+    plugins,
   };
 };
