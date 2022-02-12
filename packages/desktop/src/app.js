63b96b5225a55fc9d08d5478d8bd5c691f9bbc05
Olle Werme <olle.werme@gmail.com>
2022-02-12 19:35:28 +0100

    Extract all the things

--- packages/desktop/src/app.js
+++ packages/desktop/src/app.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { IntlProvider } from "react-intl";
-import { API_ENDPOINT } from "./constants/api";
-import { generateDummyId } from "./utils/misc";
-import { Provider as AppScopeProvider } from "./hooks/app-scope";
-import useRootReducer from "./hooks/root-reducer";
-import { useAuth, AuthProvider } from "@shades/common";
-import useServerConnection from "./hooks/server-connection";
+import Pusher from "pusher-js";
+import {
+  useAuth,
+  AuthProvider,
+  useAppScope,
+  AppScopeProvider,
+  useServerConnection,
+  ServerConnectionProvider,
+} from "@shades/common";
 import SignInScreen from "./components/sign-in-screen";
 import Channel from "./components/channel";
 import ChannelLayout from "./components/channel-layout";
@@ -18,108 +21,12 @@ const App = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const { isSignedIn, user, accessToken, authorizedFetch } = useAuth();
-  const [stateSelectors, dispatch] = useRootReducer();
-  const serverConnection = useServerConnection({
-    accessToken,
-    userId: user?.id,
-  });
-
-  const sendServerMessage = React.useCallback(
-    (name, data) => {
-      const messageSent = serverConnection.send(name, data);
-      // Dispatch a client action if the message was successfully sent
-      if (messageSent) dispatch({ type: name, data });
-      return messageSent;
-    },
-    [dispatch, serverConnection]
-  );
-
-  const fetchUserData = React.useCallback(() => {
-    sendServerMessage("request-user-data");
-  }, [sendServerMessage]);
-
-  const fetchMessages = React.useCallback(
-    ({ channelId }) =>
-      authorizedFetch(`/channels/${channelId}/messages`).then((messages) => {
-        dispatch({ type: "messages-fetched", messages });
-        return messages;
-      }),
-    [authorizedFetch, dispatch]
-  );
-
-  const createServer = React.useCallback(
-    ({ name }) =>
-      authorizedFetch("/servers", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name }),
-      }).then((res) => {
-        // TODO
-        fetchUserData();
-        return res;
-      }),
-    [authorizedFetch, fetchUserData]
-  );
-
-  const markChannelRead = React.useCallback(
-    ({ channelId, date = new Date() }) => {
-      sendServerMessage("mark-channel-read", { channelId, date });
-    },
-    [sendServerMessage]
-  );
-
-  const createMessage = React.useCallback(
-    async ({ server, channel, content }) => {
-      // TODO: Less hacky optimistc UI
-      const message = { server, channel, content };
-      const dummyId = generateDummyId();
-
-      dispatch({
-        type: "message-create-request-sent",
-        message: {
-          ...message,
-          id: dummyId,
-          created_at: new Date().toISOString(),
-          author: user.id,
-        },
-      });
-
-      return authorizedFetch("/messages", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(message),
-      }).then((message) => {
-        dispatch({
-          type: "message-create-request-successful",
-          message,
-          optimisticEntryId: dummyId,
-        });
-        markChannelRead({ channelId: channel });
-        return message;
-      });
-    },
-    [authorizedFetch, user, markChannelRead, dispatch]
-  );
-
-  const createChannel = React.useCallback(
-    ({ name, kind, server }) =>
-      authorizedFetch("/channels", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name, kind, server }),
-      }).then((res) => {
-        // TODO
-        fetchUserData();
-        return res;
-      }),
-    [authorizedFetch, fetchUserData]
-  );
+  const { status: authStatus, user } = useAuth();
+  const serverConnection = useServerConnection();
+  const { actions } = useAppScope();
 
   React.useEffect(() => {
     const handler = (name, data) => {
-      dispatch({ type: ["server-event", name].join(":"), data, user });
-
       switch (name) {
         case "user-data": {
           const server = data.servers[0];
@@ -134,7 +41,7 @@ const App = () => {
             // Temporary ofc
             const defaultServerName = `${user.display_name}’a server`;
             const serverName = prompt("Name your server", defaultServerName);
-            createServer({ name: serverName ?? defaultServerName });
+            actions.createServer({ name: serverName ?? defaultServerName });
             break;
           }
 
@@ -145,13 +52,15 @@ const App = () => {
               "Create your first channel",
               defaultChannelName
             );
-            createChannel({
-              name: channelName ?? defaultChannelName,
-              kind: "server",
-              server: server.id,
-            }).then((channel) => {
-              redirectToChannel(channel.id);
-            });
+            actions
+              .createChannel({
+                name: channelName ?? defaultChannelName,
+                kind: "server",
+                server: server.id,
+              })
+              .then((channel) => {
+                redirectToChannel(channel.id);
+              });
             break;
           }
 
@@ -167,46 +76,16 @@ const App = () => {
     return () => {
       removeListener();
     };
-  }, [
-    serverConnection,
-    user,
-    dispatch,
-    createChannel,
-    createServer,
-    location.pathname,
-    navigate,
-  ]);
-
-  const actions = React.useMemo(
-    () => ({
-      fetchUserData,
-      fetchMessages,
-      createServer,
-      createChannel,
-      createMessage,
-      markChannelRead,
-    }),
-    [
-      fetchUserData,
-      fetchMessages,
-      createServer,
-      createChannel,
-      createMessage,
-      markChannelRead,
-    ]
-  );
+  }, [serverConnection, user, actions, location.pathname, navigate]);
 
   return (
     <>
       {isNative && <TitleBar />}
 
-      {isSignedIn ? (
-        <AppScopeProvider
-          value={{
-            state: stateSelectors,
-            actions,
-          }}
-        >
+      {
+        authStatus === "not-authenticated" ? (
+          <SignInScreen />
+        ) : authStatus === "authenticated" ? (
           <Routes>
             <Route element={<ChannelLayout />}>
               <Route
@@ -214,12 +93,11 @@ const App = () => {
                 element={<Channel />}
               />
             </Route>
+            <Route path="/login" element={<SignInScreen />} />
             <Route path="*" element={null} />
           </Routes>
-        </AppScopeProvider>
-      ) : (
-        <SignInScreen />
-      )}
+        ) : null // Loading
+      }
     </>
   );
 };
@@ -227,8 +105,15 @@ const App = () => {
 export default function Root() {
   return (
     <IntlProvider locale="en">
-      <AuthProvider apiBase={API_ENDPOINT}>
-        <App />
+      <AuthProvider apiOrigin={process.env.API_ENDPOINT}>
+        <ServerConnectionProvider
+          Pusher={Pusher}
+          pusherKey={process.env.PUSHER_KEY}
+        >
+          <AppScopeProvider>
+            <App />
+          </AppScopeProvider>
+        </ServerConnectionProvider>
       </AuthProvider>
     </IntlProvider>
   );
--- packages/desktop/src/hooks/app-scope.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const GlobalStateContext = React.createContext({});
-
-const useGlobalState = () => React.useContext(GlobalStateContext);
-
-export const Provider = GlobalStateContext.Provider;
-
-export default useGlobalState;
