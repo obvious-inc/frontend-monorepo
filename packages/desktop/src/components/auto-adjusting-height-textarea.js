b5f25838e5753e5bf8aeca5cba627b05bc565b0c
Olle Werme <olle.werme@gmail.com>
2022-02-16 13:21:26 +0100

    Fix: adjust to changes not triggered by change events

--- packages/desktop/src/components/auto-adjusting-height-textarea.js
+++ packages/desktop/src/components/auto-adjusting-height-textarea.js
@@ -1,24 +1,16 @@
 import React from "react";
 
-const resize = (el) => {
-  el.style.height = "inherit";
-  el.style.height = `${el.scrollHeight}px`;
-};
-
 const AutoAdjustingHeightTextarea = React.forwardRef((props, ref) => {
   React.useEffect(() => {
-    resize(ref.current);
-  }, [ref]);
+    ref.current.style.height = "inherit";
+    ref.current.style.height = `${ref.current.scrollHeight}px`;
+  }, [props.value, ref]);
 
   return (
     <textarea
       {...props}
       ref={ref}
       style={{ resize: "none", overflow: "hidden", ...props.style }}
-      onChange={(e) => {
-        if (props.onChange != null) props.onChange(e);
-        resize(e.target);
-      }}
     />
   );
 });
