ce8981d2a29c9b69581fc7f8ae75e945479a9cbf
olli <1403501+werme@users.noreply.github.com>
2022-04-22 13:47:00 +0200

    wip

--- packages/desktop/src/components/avatar.js
+++ packages/desktop/src/components/avatar.js
@@ -6,66 +6,64 @@ import generateAvatar from "../utils/avatar-generator";
 // between multiple component instances
 const cache = new Map();
 
-const Avatar = React.memo(
-  ({
-    url,
-    walletAddress,
-    size = "2rem",
-    pixelSize = 20,
-    borderRadius = "0.3rem",
-    ...props
-  }) => {
-    const avatarDataUrl = React.useMemo(() => {
-      if (url != null || walletAddress == null) return;
+const Avatar = ({
+  url,
+  walletAddress,
+  size = "2rem",
+  pixelSize = 20,
+  borderRadius = "0.3rem",
+  ...props
+}) => {
+  const avatarDataUrl = React.useMemo(() => {
+    if (url != null || walletAddress == null) return;
 
-      const size = 8;
+    const size = 8;
 
-      const cacheKey = [walletAddress, pixelSize, size].join("-");
+    const cacheKey = [walletAddress, pixelSize, size].join("-");
 
-      if (cache.has(cacheKey)) return cache.get(cacheKey);
+    if (cache.has(cacheKey)) return cache.get(cacheKey);
 
-      const avatar = generateAvatar({
-        seed: walletAddress,
-        size,
-        scale: Math.ceil((pixelSize * 2) / size),
-      });
+    const avatar = generateAvatar({
+      seed: walletAddress,
+      size,
+      scale: Math.ceil((pixelSize * 2) / size),
+    });
 
-      cache.set(cacheKey, avatar);
+    cache.set(cacheKey, avatar);
 
-      return avatar;
-    }, [url, walletAddress, pixelSize]);
+    return avatar;
+  }, [url, walletAddress, pixelSize]);
 
-    if (url === undefined)
-      return (
-        <div
-          css={(theme) =>
-            css({
-              borderRadius,
-              background: theme.colors.backgroundSecondary,
-              height: size,
-              width: size,
-            })
-          }
-          {...props}
-        />
-      );
-
+  if (url === undefined)
     return (
-      <img
-        src={url ?? avatarDataUrl}
+      <div
         css={(theme) =>
           css({
             borderRadius,
             background: theme.colors.backgroundSecondary,
             height: size,
             width: size,
-            objectFit: "cover",
           })
         }
         {...props}
       />
     );
-  }
-);
+
+  return (
+    <img
+      src={url ?? avatarDataUrl}
+      css={(theme) =>
+        css({
+          borderRadius,
+          background: theme.colors.backgroundSecondary,
+          height: size,
+          width: size,
+          objectFit: "cover",
+        })
+      }
+      {...props}
+    />
+  );
+};
 
 export default Avatar;
