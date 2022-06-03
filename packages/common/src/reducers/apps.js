1b6815b9efd69f9d31b3b59883c52d9543669c79
prego <pedropregueiro@gmail.com>
2022-06-03 11:10:32 +0200

    handle no apps state

--- packages/common/src/reducers/apps.js
+++ packages/common/src/reducers/apps.js
@@ -14,6 +14,6 @@ const entriesById = (state = {}, action) => {
   }
 };
 
-export const selectApp = (state, appId) => state.apps.entriesById[appId];
+export const selectApp = (state, appId) => state.apps?.entriesById[appId];
 
 export default combineReducers({ entriesById });
