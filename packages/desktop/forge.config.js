8a305ffc747d83a4cc195705a3e0e6291f4112d9
olli <1403501+werme@users.noreply.github.com>
2022-06-13 22:01:54 +0200

    Make electron forge work with npm workspaces

--- packages/desktop/forge.config.js
+++ packages/desktop/forge.config.js
@@ -1,5 +1,9 @@
+const bundler = require("./electron-forge-bundler.js");
+
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    prune: false,
+  },
   makers: [
     {
       name: "@electron-forge/maker-zip",
@@ -18,4 +22,14 @@ module.exports = {
       },
     },
   ],
+  hooks: {
+    // this is a workaround until we find a proper solution
+    // for running electron-forge in a mono repository
+    packageAfterCopy: async (
+      /** @type {any} */ forgeConfig,
+      /** @type {string} */ buildPath
+    ) => {
+      await bundler.bundle(__dirname, buildPath);
+    },
+  },
 };
