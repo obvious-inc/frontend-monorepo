748f3bf78405e07f6e840392f96266fed9406464
olli <1403501+werme@users.noreply.github.com>
2022-09-28 10:38:26 +0200

    wip

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,22 +1,7 @@
-// require("dotenv").config();
-
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // [
-      //   "transform-inline-environment-variables",
-      //   {
-      //     include: [
-      //       "NODE_ENV",
-      //       "PUSHER_KEY",
-      //       "API_ENDPOINT",
-      //       "WEB_APP_ENDPOINT",
-      //     ],
-      //   },
-      // ],
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
