996f394d0dba4f2e2f6ca9aa0cf86e746c23907c
olli <1403501+werme@users.noreply.github.com>
2022-09-29 09:45:41 +0200

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,29 +4,12 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: "latest",
     sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "react/prop-types": "off",
-    "react/display-name": "off",
-    "react/no-unknown-property": [2, { ignore: ["jsx"] }],
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
   },
 };
