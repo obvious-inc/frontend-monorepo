77946047552ebb2db6b69c95009c3a26402dfb05
olli <1403501+werme@users.noreply.github.com>
2022-11-01 19:45:33 +0100

    (╯°□°)╯︵ ┻━┻

--- packages/mobile/src/screens/new-group.js
+++ packages/mobile/src/screens/new-group.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { View, Text, Pressable, ScrollView, Keyboard } from "react-native";
+import { View, Text, Pressable, ScrollView } from "react-native";
 import * as Shades from "@shades/common";
-import Input from "./input";
+import Input from "../components/input";
 import { useAsyncDismissKeyboard } from "./new-chat";
 import { HorizontalUserListItem } from "./new-closed-channel";
 
