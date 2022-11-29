e9b5746497cc4f803d7c8494f31a48f29a57f82d
olli <1403501+werme@users.noreply.github.com>
2022-11-29 12:10:18 +0100

    Add beautiful privacy policy

--- packages/landing/app/layout.js
+++ packages/landing/app/layout.js
@@ -4,7 +4,21 @@ import "../global.css";
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head></head>
+      <head>
+        <title>NewShades</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon-192x192.png"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
