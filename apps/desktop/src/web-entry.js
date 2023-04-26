8d1cde37f54df771557ae385921345457b683ed4
olli <1403501+werme@users.noreply.github.com>
2023-04-26 13:40:06 +0200

    Try Vercel analytics

--- apps/desktop/src/web-entry.js
+++ apps/desktop/src/web-entry.js
@@ -2,6 +2,7 @@ import "./polyfills";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import * as Sentry from "@sentry/react";
+import { inject as injectVercelAnalytics } from "@vercel/analytics";
 import App from "./app";
 import "./reset.css";
 import "./index.css";
@@ -25,6 +26,7 @@ const unregisterServiceWorker = () => {
 if (process.env.NODE_ENV === "production") {
   Sentry.init({ dsn: process.env.SENTRY_DSN });
   registerServiceWorker();
+  injectVercelAnalytics();
 } else {
   unregisterServiceWorker();
 }
