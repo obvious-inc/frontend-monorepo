8adb036188a4ba9b9071279ffff080d57060d50f
olli <1403501+werme@users.noreply.github.com>
2023-09-19 09:42:57 +0200

    Add impersonation feature

--- apps/prechain/src/hooks/wallet.js
+++ apps/prechain/src/hooks/wallet.js
@@ -1,10 +1,19 @@
 import { useAccount, useConnect } from "wagmi";
 
+const impersonationAddress = new URLSearchParams(location.search).get(
+  "impersonate"
+);
+
 export const useWallet = () => {
   const { address } = useAccount();
   const { connect, connectors, isLoading, reset } = useConnect();
   const requestAccess = () =>
     connect({ connector: connectors.find((c) => c.ready) });
 
-  return { address, requestAccess, isLoading, reset };
+  return {
+    address: impersonationAddress ?? address,
+    requestAccess,
+    isLoading,
+    reset,
+  };
 };
