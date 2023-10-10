01acbdd194bb6a043b0afca72c069d475cba4b90
prego <pedropregueiro@gmail.com>
2023-10-10 21:53:11 +0200

    proper navbar

--- apps/farcord/src/components/notifications-screen.js
+++ apps/farcord/src/components/notifications-screen.js
@@ -13,9 +13,11 @@ import {
 } from "../hooks/notifications.js";
 import NotificationItem from "./notification.js";
 import { usePreviousValue } from "../hooks/previous-value.js";
+import NotificationsNavBar from "./notifications-navbar.js";
 
 const NotificationsView = () => {
   const notificationsContainerRef = React.useRef();
+  const scrollContainerRef = React.useRef();
 
   const { fid } = useFarcasterAccount();
   const notifications = useSortedByDateNotificationsByFid(fid);
@@ -57,43 +59,82 @@ const NotificationsView = () => {
         })
       }
     >
+      <NotificationsNavBar />
+
       <div
-        ref={notificationsContainerRef}
-        role="list"
-        css={(t) =>
-          css({
-            // minHeight: 0,
-            fontSize: t.text.sizes.normal,
-            fontWeight: "400",
-            "--avatar-size": t.messages.avatarSize,
-            "--gutter-size": t.messages.gutterSize,
-            "--gutter-size-compact": t.messages.gutterSizeCompact,
-            ".channel-message-container": {
-              "--color-optimistic": t.colors.textMuted,
-              "--bg-highlight": t.colors.messageBackgroundModifierHighlight,
-              "--bg-focus": t.colors.messageBackgroundModifierFocus,
-              background: "var(--background, transparent)",
-              padding: "var(--padding)",
-              borderRadius: "var(--border-radius, 0)",
-              color: "var(--color, ${t.colors.textNormal})",
-              // position: "relative",
-              lineHeight: 1.46668,
-              userSelect: "text",
-            },
-          })
-        }
+        css={css({
+          position: "relative",
+          flex: 1,
+          display: "flex",
+          minHeight: 0,
+          minWidth: 0,
+        })}
       >
-        {notifications && notifications.length > 0 && (
-          <>
-            {notifications.map((notification) => (
-              <NotificationItem
-                key={`${notification.type}-${notification.hash}`}
-                notification={notification}
-                unseen={notification.timestamp > prevNotifsLastSeenAt}
-              />
-            ))}
-          </>
-        )}
+        <div
+          ref={scrollContainerRef}
+          css={css({
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            overflowY: "scroll",
+            overflowX: "hidden",
+            minHeight: 0,
+            flex: 1,
+            overflowAnchor: "none",
+          })}
+        >
+          <div
+            css={css({
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "stretch",
+              minHeight: "100%",
+            })}
+          >
+            <div
+              ref={notificationsContainerRef}
+              role="list"
+              css={(t) =>
+                css({
+                  minHeight: 0,
+                  fontSize: t.text.sizes.normal,
+                  fontWeight: "400",
+                  "--avatar-size": t.messages.avatarSize,
+                  "--gutter-size": t.messages.gutterSize,
+                  "--gutter-size-compact": t.messages.gutterSizeCompact,
+                  ".channel-message-container": {
+                    "--color-optimistic": t.colors.textMuted,
+                    "--bg-highlight":
+                      t.colors.messageBackgroundModifierHighlight,
+                    "--bg-focus": t.colors.messageBackgroundModifierFocus,
+                    background: "var(--background, transparent)",
+                    padding: "var(--padding)",
+                    borderRadius: "var(--border-radius, 0)",
+                    color: "var(--color, ${t.colors.textNormal})",
+                    position: "relative",
+                    lineHeight: 1.46668,
+                    userSelect: "text",
+                  },
+                })
+              }
+            >
+              {notifications && notifications.length > 0 && (
+                <>
+                  {notifications.map((notification) => (
+                    <NotificationItem
+                      key={`${notification.type}-${notification.hash}`}
+                      notification={notification}
+                      unseen={notification.timestamp > prevNotifsLastSeenAt}
+                    />
+                  ))}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
       <div css={css({ height: "2rem" })}></div>
     </div>
