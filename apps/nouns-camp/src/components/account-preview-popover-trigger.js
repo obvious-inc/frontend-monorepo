8c3c0220e0e01e02466ab8f8afa776b7a02139e7
olli <1403501+werme@users.noreply.github.com>
2023-12-11 14:55:43 +0100

    Add button to make account linking clearer

--- apps/nouns-camp/src/components/account-preview-popover-trigger.js
+++ apps/nouns-camp/src/components/account-preview-popover-trigger.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEnsName, useEnsAvatar } from "wagmi";
+import { Link as RouterLink } from "react-router-dom";
 import { css } from "@emotion/react";
 import { useAccountDisplayName } from "@shades/common/app";
 import Button from "@shades/ui-web/button";
@@ -17,7 +18,6 @@ const AccountPreviewPopoverTrigger = React.forwardRef(
       showAvatar = false,
       variant: buttonVariant = "link",
       popoverPlacement = "top",
-      accountActions = [],
       children,
       ...props
     },
@@ -78,94 +78,101 @@ const AccountPreviewPopoverTrigger = React.forwardRef(
       <Popover.Root placement={popoverPlacement} {...props}>
         <Popover.Trigger asChild>{renderTrigger()}</Popover.Trigger>
         <Popover.Content>
-          <AccountPreview
-            accountAddress={accountAddress}
-            actions={accountActions}
-          />
+          <AccountPreview accountAddress={accountAddress} />
         </Popover.Content>
       </Popover.Root>
     );
   }
 );
 
-const AccountPreview = React.forwardRef(
-  ({ accountAddress, actions = [] }, ref) => {
-    const delegate = useDelegate(accountAddress);
+const AccountPreview = React.forwardRef(({ accountAddress }, ref) => {
+  const delegate = useDelegate(accountAddress);
 
-    const { displayName, truncatedAddress } =
-      useAccountDisplayName(accountAddress);
+  const { displayName, truncatedAddress } =
+    useAccountDisplayName(accountAddress);
 
-    const { data: ensName } = useEnsName({ address: accountAddress });
-    const { data: ensAvatarUrl } = useEnsAvatar({
-      name: ensName,
-      enabled: ensName != null,
-    });
+  const { data: ensName } = useEnsName({ address: accountAddress });
+  const { data: ensAvatarUrl } = useEnsAvatar({
+    name: ensName,
+    enabled: ensName != null,
+  });
 
-    return (
+  const accountLink = `/campers/${ensName ?? accountAddress}`;
+
+  return (
+    <div
+      ref={ref}
+      css={css({
+        width: "32rem",
+        minWidth: 0,
+        borderRadius: "0.4rem",
+        overflow: "hidden",
+      })}
+    >
       <div
-        ref={ref}
+        css={(t) =>
+          css({
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.8rem",
+            padding: "1rem 1.2rem",
+            borderBottom: "0.1rem solid",
+            borderColor: t.colors.borderLighter,
+            h2: {
+              fontWeight: "400",
+              fontSize: t.text.sizes.small,
+              color: t.colors.textDimmed,
+            },
+          })
+        }
+      >
+        <h2>
+          {delegate?.nounsRepresented?.length > 0
+            ? "Nouns represented"
+            : "No delegation currently"}
+        </h2>
+        {delegate?.nounsRepresented.length > 0 && (
+          <div
+            css={(t) =>
+              css({
+                display: "flex",
+                gap: "1.2rem",
+                flexWrap: "wrap",
+                justifyContent: "flex-start",
+                paddingTop: "0.2rem",
+                marginBottom: "-0.2rem",
+                "[data-id]": {
+                  fontSize: t.text.sizes.tiny,
+                  color: t.colors.textDimmed,
+                  margin: "0.2rem 0 0",
+                  textAlign: "center",
+                },
+              })
+            }
+          >
+            {delegate.nounsRepresented.map((n) => (
+              <div key={n.id}>
+                <NounAvatar id={n.id} seed={n.seed} size="3.2rem" />
+                <div data-id>{n.id}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div
         css={css({
-          width: "32rem",
-          minWidth: 0,
-          borderRadius: "0.4rem",
-          overflow: "hidden",
+          display: "flex",
+          alignItems: "center",
+          padding: "1rem 1.2rem",
+          gap: "1rem",
         })}
       >
         <div
-          css={(t) =>
-            css({
-              display: "flex",
-              flexDirection: "column",
-              gap: "0.8rem",
-              padding: "1rem 1.2rem",
-              borderBottom: "0.1rem solid",
-              borderColor: t.colors.borderLighter,
-              h2: {
-                fontWeight: "400",
-                fontSize: t.text.sizes.small,
-                color: t.colors.textDimmed,
-              },
-            })
-          }
-        >
-          <h2>
-            {delegate?.nounsRepresented?.length > 0
-              ? "Nouns represented"
-              : "No delegation currently"}
-          </h2>
-          {delegate?.nounsRepresented.length > 0 && (
-            <div
-              css={(t) =>
-                css({
-                  display: "flex",
-                  gap: "1.2rem",
-                  flexWrap: "wrap",
-                  justifyContent: "flex-start",
-                  paddingTop: "0.2rem",
-                  marginBottom: "-0.2rem",
-                  "[data-id]": {
-                    fontSize: t.text.sizes.tiny,
-                    color: t.colors.textDimmed,
-                    margin: "0.2rem 0 0",
-                    textAlign: "center",
-                  },
-                })
-              }
-            >
-              {delegate.nounsRepresented.map((n) => (
-                <div key={n.id}>
-                  <NounAvatar id={n.id} seed={n.seed} size="3.2rem" />
-                  <div data-id>{n.id}</div>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-        <div
           css={css({
+            flex: 1,
+            minWidth: 0,
             display: "flex",
             alignItems: "center",
-            padding: "1rem 1.2rem",
             gap: "1rem",
           })}
         >
@@ -185,7 +192,7 @@ const AccountPreview = React.forwardRef(
           )}
           <div style={{ flex: 1, minWidth: 0, lineHeight: 1.25 }}>
             <Link
-              to={`/campers/${accountAddress}`}
+              to={accountLink}
               css={css({
                 color: "inherit",
                 textDecoration: "none",
@@ -201,6 +208,9 @@ const AccountPreview = React.forwardRef(
                   data-hover-underline={displayName === truncatedAddress}
                   css={(t) =>
                     css({
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
                       color: t.colors.header,
                       fontSize: t.text.sizes.large,
                       fontWeight: t.text.weights.header,
@@ -230,71 +240,12 @@ const AccountPreview = React.forwardRef(
             </Link>
           </div>
         </div>
-
-        {actions.length > 0 && (
-          <div
-            css={(t) =>
-              css({
-                padding: "1.2rem",
-                borderTop: "0.1rem solid",
-                borderColor: t.colors.borderLighter,
-              })
-            }
-          >
-            {/* {user.description != null && ( */}
-            {/*   <div css={(t) => css({ fontSize: t.text.sizes.base })}> */}
-            {/*     {user.description} */}
-            {/*   </div> */}
-            {/* )} */}
-
-            {actions.length > 0 && (
-              <div
-                css={css({
-                  display: "grid",
-                  gridTemplateColumns: "repeat(2, minmax(0,1fr))",
-                  gridGap: "1.2rem",
-                })}
-                // style={{
-                //   marginTop: user.description == null ? undefined : "1.2rem",
-                // }}
-              >
-                {actions.map((a, i) => (
-                  <Button
-                    key={i}
-                    size="small"
-                    onClick={a.onSelect}
-                    style={{ whiteSpace: "nowrap", textOverflow: "ellipsis" }}
-                  >
-                    {a.label}
-                  </Button>
-                ))}
-
-                {/* {isMe ? ( */}
-                {/*   <Button */}
-                {/*     size="small" */}
-                {/*     onClick={() => { */}
-                {/*       dismiss(); */}
-                {/*       openEditProfileDialog(); */}
-                {/*     }} */}
-                {/*   > */}
-                {/*     Edit profile */}
-                {/*   </Button> */}
-                {/* ) : ( */}
-                {/*   <Button */}
-                {/*     size="small" */}
-                {/*     onClick={copyWalletAddress} */}
-                {/*     style={{ whiteSpace: "nowrap", textOverflow: "ellipsis" }} */}
-                {/*   > */}
-                {/*     {textCopied ? "Address copied" : "Copy address"} */}
-                {/*   </Button> */}
-                {/* )} */}
-              </div>
-            )}
-          </div>
-        )}
+        <Button size="default" component={RouterLink} to={accountLink}>
+          View profile
+        </Button>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
 
 export default AccountPreviewPopoverTrigger;
