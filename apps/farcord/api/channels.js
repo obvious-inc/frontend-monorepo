aa2a78d335618112f0272f899bde9312aa923114
prego <pedropregueiro@gmail.com>
2023-12-25 19:13:42 +0100

    use Warpcast api to fetch channels instead of gh static list

--- apps/farcord/api/channels.js
+++ apps/farcord/api/channels.js
@@ -1,25 +1,26 @@
-const NEYNAR_FARCASTER_CHANNELS_STATIC_LIST =
-  "https://raw.githubusercontent.com/neynarxyz/farcaster-channels/main/warpcast.json";
+const WARPCAST_CHANNELS_STATIC_LIST =
+  "https://api.warpcast.com/v2/all-channels";
 
 const WARPCAST_CHANNELS_INFO_ENDPOINT =
   "https://client.warpcast.com/v2/channel";
 
 export default async function handler(request, response) {
-  const channels = await fetch(NEYNAR_FARCASTER_CHANNELS_STATIC_LIST)
+  const channels = await fetch(WARPCAST_CHANNELS_STATIC_LIST)
     .then(async (res) => {
       if (res.ok) return res.json();
       return Promise.reject(new Error(res.statusText));
     })
     .then(async (data) => {
+      const channels = data?.result?.channels;
+      if (!channels) return [];
+
       return Promise.all(
-        data.map((channel) =>
-          fetch(WARPCAST_CHANNELS_INFO_ENDPOINT + "?key=" + channel.channel_id)
+        channels.map((channel) =>
+          fetch(WARPCAST_CHANNELS_INFO_ENDPOINT + "?key=" + channel.id)
             .then((res) => {
               if (res.ok) return res.json();
               else {
-                console.error(
-                  "Error fetching channel info for " + channel.channel_id
-                );
+                console.error("Error fetching channel info for " + channel.id);
                 return null;
               }
             })
@@ -27,8 +28,8 @@ export default async function handler(request, response) {
               if (!body) return;
               const warpcastChannel = body.result.channel;
               return {
-                id: channel.channel_id,
-                parentUrl: channel.parent_url,
+                id: channel.id,
+                parentUrl: channel.url,
                 name: warpcastChannel.name,
                 imageUrl: warpcastChannel.fastImageUrl,
                 followerCount: warpcastChannel.followerCount,
