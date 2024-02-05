a6f59cb06a654561791c048c999d7859c398f7ff
prego <pedropregueiro@gmail.com>
2024-02-05 11:22:06 +0100

    move json file

--- apps/farcord/api/channels.js
+++ apps/farcord/api/channels.js
@@ -11,7 +11,7 @@ export default async function handler(_, response) {
   let fileChannels = [];
 
   try {
-    const filePath = path.join(process.cwd(), "files", "all-channels.json");
+    const filePath = path.join(process.cwd(), "all-channels.json");
     const fileContents = readFileSync
       ? readFileSync(filePath, "utf8")
       : require
