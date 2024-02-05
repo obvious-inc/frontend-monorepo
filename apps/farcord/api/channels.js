e603f79ac419a537e3f4152c80be24d8e1b88f1c
prego <pedropregueiro@gmail.com>
2024-02-05 11:16:12 +0100

    load from file first and then fetch from warpcast

--- apps/farcord/api/channels.js
+++ apps/farcord/api/channels.js
@@ -8,6 +8,28 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export default async function handler(_, response) {
+  let fileChannels = [];
+
+  try {
+    const filePath = path.join(process.cwd(), "files", "all-channels.json");
+    const fileContents = readFileSync
+      ? readFileSync(filePath, "utf8")
+      : require
+      ? require(filePath)
+      : null;
+
+    if (fileContents) {
+      fileChannels = JSON.parse(fileContents);
+    }
+  } catch (e) {
+    console.error("Error reading backup JSON file: ", e);
+  }
+
+  const fileChannelsById = fileChannels.reduce((acc, channel) => {
+    acc[channel.id] = channel;
+    return acc;
+  }, {});
+
   const channels = await fetch(WARPCAST_CHANNELS_STATIC_LIST)
     .then(async (res) => {
       if (res.ok) return res.json();
@@ -18,8 +40,13 @@ export default async function handler(_, response) {
       if (!channels) return [];
 
       return Promise.all(
-        channels.map((channel) =>
-          fetch(WARPCAST_CHANNELS_INFO_ENDPOINT + "?key=" + channel.id)
+        channels.map((channel) => {
+          // avoid fetching if channel id and url are already in the backup
+          if (fileChannelsById?.[channel.id]?.url === channel.url) {
+            return fileChannelsById[channel.id];
+          }
+
+          return fetch(WARPCAST_CHANNELS_INFO_ENDPOINT + "?key=" + channel.id)
             .then((res) => {
               if (res.ok) return res.json();
               else {
@@ -41,8 +68,8 @@ export default async function handler(_, response) {
             .catch((e) => {
               console.error("Error fetching channel info for " + channel.id, e);
               return null;
-            })
-        )
+            });
+        })
       ).then((result) => {
         // filter undefined keys
         return result.filter((c) => c);
@@ -50,22 +77,6 @@ export default async function handler(_, response) {
     })
     .catch((e) => {
       console.error("Error fetching warpcast channels, using JSON backup: ", e);
-
-      try {
-        const filePath = path.join(process.cwd(), "files", "all-channels.json");
-        const fileContents = readFileSync
-          ? readFileSync(filePath, "utf8")
-          : require
-          ? require(filePath)
-          : null;
-
-        if (fileContents) {
-          return JSON.parse(fileContents);
-        }
-      } catch (e) {
-        console.error("Error reading backup JSON file: ", e);
-        return [];
-      }
 
       return [];
     });
