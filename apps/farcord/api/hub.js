86ec776bc863b2f48dd8445f5d3628a6a89f9785
prego <pedropregueiro@gmail.com>
2024-02-08 20:33:37 +0100

    logging ftw

--- apps/farcord/api/hub.js
+++ apps/farcord/api/hub.js
@@ -3,9 +3,15 @@ export const config = {
 };
 
 export default async function handler(request) {
+  console.log("redirecting request...");
   // redirect request to the hub and add the api key header
-  const requestPath = request.path.replace(/^\/hub/, "");
+  console.log("og path", request.path);
+  const requestPath = request.path?.replace(/^\/hub/, "");
+  console.log("new path", requestPath);
+
   return fetch(process.env.FARCASTER_HUB_HTTP_ENDPOINT + requestPath, {
+    method: request.method,
+    body: request.body,
     headers: {
       api_key: process.env.FARCASTER_HUB_API_KEY,
     },
