96efcc45550ed93ef2df8524cc3742db7987b36e
prego <pedropregueiro@gmail.com>
2024-02-09 11:31:26 +0100

    one more time with feeling

--- apps/farcord/api/hub.js
+++ apps/farcord/api/hub.js
@@ -10,13 +10,29 @@ export default async function handler(request, response) {
     request.headers,
     request.body
   );
-  return fetch(process.env.FARCASTER_HUB_HTTP_ENDPOINT + request.url, {
+
+  // remove path from query parameters and use as part of URL
+  const urlParams = new URLSearchParams(request.url.split("?")[1]);
+  console.log("url params", urlParams.toString());
+
+  const path = urlParams.get("path");
+  urlParams.delete("path");
+
+  const url = process.env.FARCASTER_HUB_HTTP_ENDPOINT + path + "?" + urlParams;
+
+  console.log("url", url);
+
+  return fetch(url, {
     method: request.method,
     headers: request.headers,
     body: request.body,
   })
     .then((res) => {
-      console.log("result", res);
+      console.log("result", res.status, res.statusText);
+      if (!res.ok) {
+        return Promise.reject(new Error(res.statusText));
+      }
+
       return res.json();
     })
     .then((data) => {
