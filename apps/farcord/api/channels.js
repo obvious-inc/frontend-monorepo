2876fa72ef7ef390091b60ca801117117d8d7ae0
prego <pedropregueiro@gmail.com>
2024-02-10 13:50:30 +0100

    use neynar apis for fetching all channels sorted by cast count

--- apps/farcord/api/channels.js
+++ apps/farcord/api/channels.js
@@ -1,75 +1,35 @@
-const WARPCAST_CHANNELS_STATIC_LIST =
-  "https://api.warpcast.com/v2/all-channels";
+const NEYNAR_V2_ENDPOINT = "https://api.neynar.com/v2/farcaster";
 
-const WARPCAST_CHANNELS_INFO_ENDPOINT =
-  "https://client.warpcast.com/v2/channel";
+const fetchTrendingChannels = async () => {
+  let params = new URLSearchParams({
+    api_key: process.env.FARCASTER_HUB_API_KEY,
+    time_window: "7d",
+  });
 
-import { readFileSync } from "fs";
-import path from "path";
-
-export default async function handler(_, response) {
-  const channels = await fetch(WARPCAST_CHANNELS_STATIC_LIST)
-    .then(async (res) => {
-      if (res.ok) return res.json();
-      return Promise.reject(new Error(res.statusText));
+  return fetch(NEYNAR_V2_ENDPOINT + "/channel/trending?" + params)
+    .then((result) => {
+      return result.json();
     })
-    .then(async (data) => {
-      const channels = data?.result?.channels;
-      if (!channels) return [];
-
-      return Promise.all(
-        channels.map((channel) =>
-          fetch(WARPCAST_CHANNELS_INFO_ENDPOINT + "?key=" + channel.id)
-            .then((res) => {
-              if (res.ok) return res.json();
-              else {
-                throw new Error(res.statusText);
-              }
-            })
-            .then((body) => {
-              if (!body) return;
-              const warpcastChannel = body.result.channel;
-              return {
-                id: channel.id,
-                parentUrl: channel.url,
-                name: warpcastChannel.name,
-                imageUrl: warpcastChannel.fastImageUrl,
-                followerCount: warpcastChannel.followerCount,
-                description: warpcastChannel.description,
-              };
-            })
-            .catch((e) => {
-              console.error("Error fetching channel info for " + channel.id, e);
-              return null;
-            })
-        )
-      ).then((result) => {
-        // filter undefined keys
-        return result.filter((c) => c);
+    .then((data) => {
+      return data.channels;
+    })
+    .then((channels) => {
+      return channels.map((channelObject) => {
+        return {
+          ...channelObject.channel,
+          parentUrl: channelObject.channel.parent_url,
+          imageUrl: channelObject.channel.image_url,
+          castCount: Number(channelObject["cast_count_7d"]),
+        };
       });
     })
-    .catch((e) => {
-      console.error("Error fetching warpcast channels, using JSON backup: ", e);
-
-      try {
-        const filePath = path.join(process.cwd(), "files", "all-channels.json");
-        const fileContents = readFileSync
-          ? readFileSync(filePath, "utf8")
-          : require
-          ? require(filePath)
-          : null;
-
-        if (fileContents) {
-          return JSON.parse(fileContents);
-        }
-      } catch (e) {
-        console.error("Error reading backup JSON file: ", e);
-        return [];
-      }
-
-      return [];
+    .catch((err) => {
+      throw err;
     });
+};
 
+export default async function handler(_, response) {
+  const trendingChannels = await fetchTrendingChannels();
   response.setHeader("Cache-Control", "s-maxage=86400");
-  response.status(200).json({ channels: channels });
+  return response.status(200).json({ channels: trendingChannels });
 }
