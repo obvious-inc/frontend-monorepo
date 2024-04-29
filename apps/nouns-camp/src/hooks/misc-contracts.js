6b9ccd6af82a76a26db7d0f5b8b7c9d048ad7a2e
Pedro Pregueiro (prego) <pedropregueiro@gmail.com>
2024-04-29 10:12:46 +0200

    Use parsed abi to improve first load performance (#79)
    Replace all parseAbi calls with the parsed abi instead, to try and optimize performance a bit.

--- apps/nouns-camp/src/hooks/misc-contracts.js
+++ apps/nouns-camp/src/hooks/misc-contracts.js
@@ -1,5 +1,3 @@
-import { parseAbi } from "viem";
-
 import { useReadContract } from "wagmi";
 import { resolveIdentifier } from "../contracts.js";
 import useChainId from "./chain-id.js";
@@ -9,9 +7,17 @@ export const useTokenBuyerEthNeeded = (additionalUsdcTokens) => {
 
   const { data } = useReadContract({
     address: resolveIdentifier(chainId, "token-buyer").address,
-    abi: parseAbi([
-      "function ethNeeded(uint256, uint256) public view returns (uint256)",
-    ]),
+    abi: [
+      {
+        inputs: [
+          { name: "additionalTokens", type: "uint256" },
+          { name: "bufferBPs", type: "uint256" },
+        ],
+        name: "ethNeeded",
+        outputs: [{ type: "uint256" }],
+        type: "function",
+      },
+    ],
     functionName: "ethNeeded",
     args: [additionalUsdcTokens, 5_000],
     query: {
