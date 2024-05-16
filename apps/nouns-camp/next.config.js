8731fb8037b5ed97cd9189e11d41c54c9885d20a
olli <1403501+werme@users.noreply.github.com>
2024-05-16 14:24:49 +0200

    Remove prop-house support

--- apps/nouns-camp/next.config.js
+++ apps/nouns-camp/next.config.js
@@ -39,14 +39,6 @@ const withSentry = (config) =>
 const BUILD_ID = process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7) ?? "dev";
 
 const ignoredModules = [
-  // @prophouse/sdk
-  "fs",
-  "http",
-  "https",
-  "os",
-  "stream",
-  "tty",
-  "zlib",
   // wagmi / @walletconnect
   "pino-pretty",
   // wagmi / @metamask
