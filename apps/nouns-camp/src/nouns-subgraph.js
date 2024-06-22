104ea29e4b9a78e85db3082263cee8adf75f16e7
Milad Nekofar <milad@nekofar.com>
2024-06-22 18:54:53 +0400

    refactor(nouns-subgraph): simplify conditionals and add cache control
    To improve code readability, the condition check for determining server status is declared once and reused. Also, cache control has been added, with 'no-cache' for server and 'default' for client, enhancing performance.

--- apps/nouns-camp/src/nouns-subgraph.js
+++ apps/nouns-camp/src/nouns-subgraph.js
@@ -92,8 +92,7 @@ export const FULL_PROPOSAL_FIELDS = `
   # signers { id }
     votes { ...VoteFields }
   # feedbackPosts { ...ProposalFeedbackFields }
-}
-`;
+  }`;
 
 export const CANDIDATE_CONTENT_SIGNATURE_FIELDS = `
   fragment CandidateContentSignatureFields on ProposalCandidateSignature {
@@ -408,10 +407,12 @@ export const subgraphFetch = async ({
   query,
   variables,
 }) => {
+  const isServer = typeof window === "undefined";
+
   const url = (() => {
     if (endpoint != null) return endpoint;
     if (customSubgraphEndpoint != null) return customSubgraphEndpoint;
-    if (typeof window === "undefined") return SERVER_URL;
+    if (isServer) return SERVER_URL;
     return CLIENT_URL;
   })();
 
@@ -421,6 +422,7 @@ export const subgraphFetch = async ({
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ operationName, query, variables }),
+    cache: isServer ? "no-cache" : "default",
   });
   if (!response.ok) {
     console.error("Unsuccessful subgraph request", {
