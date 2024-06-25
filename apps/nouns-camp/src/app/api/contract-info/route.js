cace159264d2cc86dbcb28829a57235c804786ee
Milad Nekofar <milad@nekofar.com>
2024-06-25 10:24:50 +0400

    revert: add log to fetch Etherscan request URL
    This reverts commit 62ddbb2743874a48ee3b5cf11f9ed1b0827ed98a.

--- apps/nouns-camp/src/app/api/contract-info/route.js
+++ apps/nouns-camp/src/app/api/contract-info/route.js
@@ -20,7 +20,6 @@ const getEtherscanEndpointUrl = (chainId) => {
 const etherscanRequest = (query) => {
   const searchParams = new URLSearchParams(query);
   const url = getEtherscanEndpointUrl(CHAIN_ID);
-  console.log(url);
   return new Request(`${url}&${searchParams}`);
 };
 
