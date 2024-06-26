da976a9e5100912f67e3701098e52ccd0051c3ca
Milad Nekofar <milad@nekofar.com>
2024-06-26 17:58:05 +0400

    refactor(dao-contract): replace `adjustedTotalSupply` function with `useTotalSupply` hook
    The adjustedTotalSupply function was hard-coded and had a fixed output. This refactor replaces it with useTotalSupply, a more flexible hook that can adapt to changes.

--- apps/nouns-camp/src/hooks/dao-contract.js
+++ apps/nouns-camp/src/hooks/dao-contract.js
@@ -9,7 +9,7 @@ import useBlockNumber from "./block-number.js";
 import usePublicClient from "./public-client.js";
 import { useWallet } from "./wallet.js";
 import useRegisterEvent from "./register-event.js";
-import { useCurrentVotes } from "./token-contract.js";
+import { useCurrentVotes, useTotalSupply } from "./token-contract.js";
 
 const { address: contractAddress } = resolveIdentifier("dao");
 
@@ -117,17 +117,18 @@ export const useCurrentDynamicQuorum = ({ againstVotes = 0 } = {}) => {
 
   const blockNumber = useBlockNumber({ watch: true, cache: 20_000 });
 
-  const { data: adjustedTotalSupply } = useRead({
-    abi: [
-      {
-        inputs: [],
-        name: "adjustedTotalSupply",
-        outputs: [{ type: "uint256" }],
-        type: "function",
-      },
-    ],
-    functionName: "adjustedTotalSupply",
-  });
+  const adjustedTotalSupply = useTotalSupply()
+  // const { data: adjustedTotalSupply } = useRead({
+  //   abi: [
+  //     {
+  //       inputs: [],
+  //       name: "adjustedTotalSupply",
+  //       outputs: [{ type: "uint256" }],
+  //       type: "function",
+  //     },
+  //   ],
+  //   functionName: "adjustedTotalSupply",
+  // });
   const quorumParams = useDynamicQuorumParamsAt(blockNumber);
 
   const { data, isSuccess } = useRead({
