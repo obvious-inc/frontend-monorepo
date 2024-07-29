ae1f9ba0c393e1bdef66956cf7772efd498746e9
prego <pedropregueiro@gmail.com>
2024-07-29 14:05:05 +0200

    add etherscan link to items with transaction hash

--- apps/nouns-camp/src/components/activity-feed.js
+++ apps/nouns-camp/src/components/activity-feed.js
@@ -22,6 +22,8 @@ import NounsPreviewPopoverTrigger from "./nouns-preview-popover-trigger.js";
 import { useSaleInfo } from "../hooks/sales.js";
 import { FormattedEthWithConditionalTooltip } from "./transaction-list.js";
 import useScrollToHash from "../hooks/scroll-to-hash.js";
+import { isHex } from "viem";
+import { buildEtherscanLink } from "../utils/etherscan.js";
 
 const BODY_TRUNCATION_HEIGHT_THRESHOLD = 250;
 
@@ -152,6 +154,61 @@ const FeedItem = React.memo(
 
     const showActionBar = showReplyAction || showRepostAction || showLikeAction;
 
+    const renderTimestamp = (item) => {
+      const txHash = item.transactionHash;
+
+      const formattedDate = isHex(txHash) ? (
+        <a
+          href={buildEtherscanLink(`/tx/${txHash}`)}
+          target="_blank"
+          rel="noopener noreferrer"
+          css={css({ fontWeight: "normal !important" })}
+        >
+          <FormattedDateWithTooltip
+            tinyRelative
+            relativeDayThreshold={7}
+            month="short"
+            day="numeric"
+            year={
+              getDateYear(item.timestamp) !== getDateYear(new Date())
+                ? "numeric"
+                : undefined
+            }
+            value={item.timestamp}
+          />
+        </a>
+      ) : (
+        <FormattedDateWithTooltip
+          tinyRelative
+          relativeDayThreshold={7}
+          month="short"
+          day="numeric"
+          year={
+            getDateYear(item.timestamp) !== getDateYear(new Date())
+              ? "numeric"
+              : undefined
+          }
+          value={item.timestamp}
+        />
+      );
+
+      return (
+        <span
+          data-timestamp
+          css={(t) =>
+            css({
+              fontSize: t.text.sizes.small,
+              color: t.colors.textDimmed,
+              padding: "0.15rem 0",
+              display: "inline-block",
+            })
+          }
+        >
+          {formattedDate}
+        </span>
+      );
+    };
+
     return (
       <div
         key={item.id}
@@ -242,33 +299,7 @@ const FeedItem = React.memo(
                     <Spinner size="1rem" />
                   </div>
                 ) : (
-                  item.timestamp != null && (
-                    <span
-                      data-timestamp
-                      css={(t) =>
-                        css({
-                          fontSize: t.text.sizes.small,
-                          color: t.colors.textDimmed,
-                          padding: "0.15rem 0",
-                          display: "inline-block",
-                        })
-                      }
-                    >
-                      <FormattedDateWithTooltip
-                        tinyRelative
-                        relativeDayThreshold={7}
-                        month="short"
-                        day="numeric"
-                        year={
-                          getDateYear(item.timestamp) !==
-                          getDateYear(new Date())
-                            ? "numeric"
-                            : undefined
-                        }
-                        value={item.timestamp}
-                      />
-                    </span>
-                  )
+                  item.timestamp != null && renderTimestamp(item)
                 )}
               </div>
             </div>
---
+++ apps/nouns-camp/src/utils/etherscan.js
@@ -0,0 +1,16 @@
+import { CHAIN_ID } from "../constants/env";
+
+const getEtherscanLinkHost = () => {
+  switch (CHAIN_ID) {
+    case 1:
+      return "https://etherscan.io";
+    case 11155111:
+      return "https://sepolia.etherscan.io";
+    default:
+      throw new Error();
+  }
+};
+
+export const buildEtherscanLink = (path) => {
+  return `${getEtherscanLinkHost()}${path}`;
+};
