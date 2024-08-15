e4b55ce0533d342c546aacc6aa40d765cd75acd2
prego <pedropregueiro@gmail.com>
2024-08-15 15:52:16 +0200

    again

--- apps/farcord/api/neynar.js
+++ apps/farcord/api/neynar.js
@@ -1,12 +1,8 @@
-export const config = {
-  runtime: "edge",
-};
-
 const NEYNAR_V1_ENDPOINT = "https://api.neynar.com/v1/farcaster";
 const NEYNAR_V2_ENDPOINT = "https://api.neynar.com/v2/farcaster";
 
 // create proxy server for neynar requests, get and post
-export default async function handler(request) {
+export default async function handler(request, response) {
   let urlParams = new URLSearchParams(request.url.split("?")[1]);
   // add api key to params
   urlParams.set("api_key", process.env.FARCASTER_HUB_API_KEY);
@@ -18,13 +14,8 @@ export default async function handler(request) {
     headers: request.headers,
   });
 
-  const response = await fetch(neynarRequest);
-  const data = await response.json();
+  const neynarResponse = await fetch(neynarRequest);
+  const data = await neynarResponse.json();
 
-  return new Response(JSON.stringify(data), {
-    status: response.status,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  return response.status(neynarResponse.status).json({ data });
 }
