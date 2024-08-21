87fa625bb5618e802e498d25ca7864dcdbe092f5
olli <1403501+werme@users.noreply.github.com>
2024-08-21 13:04:03 +0200

    Encode tuple values as arrays to match data in signatures

--- apps/nouns-camp/src/components/action-dialog.js
+++ apps/nouns-camp/src/components/action-dialog.js
@@ -9,6 +9,7 @@ import {
   formatEther,
   encodeAbiParameters,
   decodeAbiParameters,
+  parseAbiItem,
 } from "viem";
 import { normalize as normalizeEnsName } from "viem/ens";
 import { css } from "@emotion/react";
@@ -183,13 +184,7 @@ const buildInitialInputState = (inputs = []) => {
     if (isArray) return [];
 
     if (input.components != null) {
-      return input.components.reduce(
-        (obj, c) => ({
-          ...obj,
-          [c.name]: buildInputState(c),
-        }),
-        {},
-      );
+      return input.components.map((c) => buildInputState(c));
     }
 
     switch (simplifyType(input.type)) {
@@ -1064,13 +1059,9 @@ const formConfigByActionType = {
       }
     },
     buildAction: ({ state }) => {
-      const selectedSignatureAbiItem = state.abi?.find(
-        (i) =>
-          createSignature({ functionName: i.name, inputTypes: i.inputs }) ===
-          state.signature,
+      const { inputs: inputTypes } = parseAbiItem(
+        `function ${state.signature}`,
       );
-
-      const { inputs: inputTypes } = selectedSignatureAbiItem;
 
       return {
         type: "custom-transaction",
@@ -1301,7 +1292,10 @@ const renderInput = (input, inputValue, setInputValue) => {
 
   if (isArray) {
     const elementType = input.type.slice(0, -2);
-    const defaultValue = input.components != null ? {} : "";
+    const defaultValue =
+      input.components != null
+        ? Array.from({ length: input.components.length })
+        : "";
     return (
       <div key={input.name} data-input>
         {labelContent != null && <Label>{labelContent}</Label>}
@@ -1367,19 +1361,19 @@ const renderInput = (input, inputValue, setInputValue) => {
         {labelContent != null && <Label>{labelContent}</Label>}
         <div data-components css={css({ display: "flex", gap: "0.4rem" })}>
           <div css={css({ flex: "1", minWidth: 0 })}>
-            {input.components.map((c) => {
-              const componentValue = inputValue?.[c.name] ?? "";
+            {input.components.map((c, componentIndex) => {
+              const componentValue = inputValue?.[componentIndex] ?? "";
               const setComponentValue = (getComponentValue) => {
                 setInputValue((currentInputValue) => {
-                  const currentComponentValue = currentInputValue?.[c.name];
+                  const currentComponentValue =
+                    currentInputValue?.[componentIndex];
                   const nextComponentValue =
                     typeof getComponentValue === "function"
                       ? getComponentValue(currentComponentValue)
                       : getComponentValue;
-                  return {
-                    ...currentInputValue,
-                    [c.name]: nextComponentValue,
-                  };
+                  const nextInputValue = [...currentInputValue];
+                  nextInputValue[componentIndex] = nextComponentValue;
+                  return nextInputValue;
                 });
               };
 
