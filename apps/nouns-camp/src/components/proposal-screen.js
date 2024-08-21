71a5d6e483e85f3fa26b4f0db193dd9d1f182c27
Milad Nekofar <milad@nekofar.com>
2024-08-22 00:04:45 +0400

    feat(proposal-screen): add `oEthAprBps` and `oEthYield`
    enhance yield calculations by including originEther APR.

--- apps/nouns-camp/src/components/proposal-screen.js
+++ apps/nouns-camp/src/components/proposal-screen.js
@@ -943,11 +943,13 @@ export const ProposalHeader = ({
 
     const stEthAprBps = BigInt(Math.round(aprs.lido * 10_000));
     const rEthAprBps = BigInt(Math.round(aprs.rocketPool * 10_000));
+    const oEthAprBps = BigInt(Math.round(aprs.originEther * 10_000));
     const stEthYield =
       ((balances.executor.steth + balances.executor.wsteth) * stEthAprBps) /
       10_000n;
     const rEthYield = ((balances.executor.reth ?? 0n) * rEthAprBps) / 10_000n;
-    const totalStakingYield = stEthYield + rEthYield;
+    const oEthYield = ((balances.executor.oeth ?? 0n) * oEthAprBps) / 10_000n;
+    const totalStakingYield = stEthYield + rEthYield + oEthYield;
 
     const projectedOneYearAuctionProceeds = avgAuctionPrice * 365n;
     const oneYearIncomeForecast =
