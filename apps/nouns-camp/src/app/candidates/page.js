69f3525faac38d957b079fc93d3679e35602d05f
olli <1403501+werme@users.noreply.github.com>
2024-08-22 11:30:42 +0200

    Add basic meta tags to browse pages

--- apps/nouns-camp/src/app/candidates/page.js
+++ apps/nouns-camp/src/app/candidates/page.js
@@ -1,5 +1,11 @@
 import ClientAppProvider from "../client-app-provider.js";
+import { build as buildMetadata } from "../../utils/metadata.js";
 import BrowseCandidatesScreen from "../../components/browse-candidates-screen.js";
+
+export const metadata = buildMetadata({
+  title: "Candidates",
+  canonicalPathname: "/candidates",
+});
 
 export default function Page() {
   return (
--- apps/nouns-camp/src/app/proposals/page.js
+++ apps/nouns-camp/src/app/proposals/page.js
@@ -1,5 +1,11 @@
 import ClientAppProvider from "../client-app-provider.js";
+import { build as buildMetadata } from "../../utils/metadata.js";
 import BrowseProposalsScreen from "../../components/browse-proposals-screen.js";
+
+export const metadata = buildMetadata({
+  title: "Proposals",
+  canonicalPathname: "/proposals",
+});
 
 export default function Page() {
   return (
