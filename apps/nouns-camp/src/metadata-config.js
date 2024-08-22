bf267d3bab008678ccf7c73abbcc0ec6b0dd72a6
olli <1403501+werme@users.noreply.github.com>
2024-08-22 11:03:13 +0200

    Paint PWA bg dark if system prefers it

--- apps/nouns-camp/src/metadata-config.js
+++ apps/nouns-camp/src/metadata-config.js
@@ -1,7 +1,8 @@
 import { APP_URL } from "./constants/env.js";
 
 export default {
-  viewportThemeColor: "#ffffff",
+  viewportLightThemeColor: "#ffffff",
+  viewportDarkThemeColor: "hsl(0 0% 10%)",
   appTitle: "Nouns Camp",
   appDescription: "A Nouns governance client",
   titleTemplate: "%s - Nouns Camp",
