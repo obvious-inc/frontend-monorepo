7900c2b75796cff0023af226cb7a16a160dfe970
prego <pedropregueiro@gmail.com>
2024-08-24 10:45:18 +0200

    handle path and query params

--- apps/farcord/api/neynar.js
+++ apps/farcord/api/neynar.js
@@ -3,11 +3,22 @@ export const config = {
 };
 
 const neynarApiKey = process.env.FARCASTER_HUB_API_KEY;
+const NEYNAR_V1_ENDPOINT = "https://api.neynar.com/v1/farcaster";
+const NEYNAR_V2_ENDPOINT = "https://api.neynar.com/v2/farcaster";
 
+// https://farcord-git-farcord-neynar-api-obvious.vercel.app/api/neynar?path=/v2/notifications&query=test
 export default async function handler(request) {
-  const path = new URL(request.url).pathname;
+  const queryParams = new URLSearchParams(request.url.split("?")[1]);
+
+  // pop path from query params
+  const path = queryParams.get("path");
   const version = path.split("/")[0];
   const remainingPath = path.split("/").slice(1).join("/");
+
+  // delete path from query params
+  queryParams.delete("path");
+
+  const remainingQuery = queryParams.toString();
 
   let body;
   try {
@@ -22,7 +33,9 @@ export default async function handler(request) {
     // handle POST requests
     return new Response(
       JSON.stringify({
-        data: { post: `version: ${version} | path: ${remainingPath}` },
+        data: {
+          post: `version: ${version} | path: ${remainingPath} | query: ${remainingQuery}`,
+        },
       }),
       {
         status: 200,
@@ -35,7 +48,7 @@ export default async function handler(request) {
     return new Response(
       JSON.stringify({
         data: {
-          get: `version: ${version} | path: ${remainingPath}`,
+          get: `version: ${version} | path: ${remainingPath}  | query: ${remainingQuery}`,
         },
       }),
       {
