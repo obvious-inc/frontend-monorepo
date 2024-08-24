3e4a6ded8cd306cc51b97965f3fcf7b1753057e8
prego <pedropregueiro@gmail.com>
2024-08-24 12:28:46 +0200

    add cache and cors to api

--- apps/farcord/api/neynar.js
+++ apps/farcord/api/neynar.js
@@ -5,21 +5,26 @@ export const config = {
 const NEYNAR_V1_ENDPOINT = "https://api.neynar.com/v1/farcaster";
 const NEYNAR_V2_ENDPOINT = "https://api.neynar.com/v2/farcaster";
 
+const CORS_HEADERS = {
+  // only allow request from farcord.com
+  "Access-Control-Allow-Origin": "https://farcord.com",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
 export default async function handler(request) {
   const queryParams = new URLSearchParams(request.url.split("?")[1]);
 
-  // pop path from query params
   const path = queryParams.get("path");
+  const cache = queryParams.get("cache");
+
   const version = path.split("/")[1];
   const remainingPath = path.split("/").slice(2).join("/");
 
   // delete path and potential api_key from query params
   queryParams.delete("path");
   queryParams.delete("api_key");
-
-  console.log("version", version);
-  console.log("path", remainingPath);
-  console.log("query", queryParams.toString());
+  queryParams.delete("cache");
 
   queryParams.append("api_key", process.env.FARCASTER_HUB_API_KEY);
 
@@ -44,61 +49,32 @@ export default async function handler(request) {
     }
   );
 
+  const responseHeaders = {
+    "Content-Type": "application/json",
+    ...CORS_HEADERS,
+  };
+
   if (!result.ok) {
     return new Response(
       JSON.stringify({
         error: {
           message: "Failed to fetch data from Neynar",
+          resultStatus: `${result.status} ${result.statusText}`,
         },
       }),
       {
         status: 400,
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: responseHeaders,
       }
     );
   }
 
   const resultBody = await result.json();
 
+  if (cache) responseHeaders["Cache-Control"] = `public, max-age=${cache}`;
+
   return new Response(JSON.stringify(resultBody), {
     status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: responseHeaders,
   });
-
-  // console.log("body", body);
-
-  // if (request.method === "POST") {
-  //   // handle POST requests
-  //   return new Response(
-  //     JSON.stringify({
-  //       data: {
-  //         post: `version: ${version} | path: ${remainingPath} | query: ${remainingQuery}`,
-  //       },
-  //     }),
-  //     {
-  //       status: 200,
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     }
-  //   );
-  // } else {
-  //   return new Response(
-  //     JSON.stringify({
-  //       data: {
-  //         get: `version: ${version} | path: ${remainingPath}  | query: ${remainingQuery}`,
-  //       },
-  //     }),
-  //     {
-  //       status: 200,
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     }
-  //   );
-  // }
 }
