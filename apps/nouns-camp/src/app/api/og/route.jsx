4dc3fec0cb783c052265ca433c9067a09142b185
prego <pedropregueiro@gmail.com>
2024-09-06 10:39:05 +0200

    remove Vercel-CDN

--- apps/nouns-camp/src/app/api/og/route.jsx
+++ apps/nouns-camp/src/app/api/og/route.jsx
@@ -786,8 +786,8 @@ export async function GET(request) {
           // https://docs.farcaster.xyz/developers/frames/advanced#making-the-initial-frame-image-dynamic
           "cache-control":
             "no-transform, s-maxage=60, max-age=60, public, immutable",
-          "Vercel-CDN-Cache-Control":
-            "no-transform, s-maxage=60, max-age=60, public, immutable",
+          //   "Vercel-CDN-Cache-Control":
+          //     "no-transform, s-maxage=60, max-age=60, public, immutable",
           "CDN-Cache-Control":
             "no-transform, s-maxage=60, max-age=60, public, immutable",
         },
