d8f7757c9cf0bc03b035eabd2500fca379f05722
olli <1403501+werme@users.noreply.github.com>
2024-09-06 08:36:43 +0200

    test

--- apps/nouns-camp/next.config.mjs
+++ apps/nouns-camp/next.config.mjs
@@ -143,7 +143,7 @@ export default withSentry(
       return config;
     },
     // https://github.com/farcasterxyz/hub-monorepo/issues/2031
-    // swcMinify: false,
+    swcMinify: false,
     experimental: {
       turbo: {
         // Ignoring modules is not a thing yet
