4546b0e8d1a7a9e6cae5e31dcc7f680ddd170e2b
Milad Nekofar <milad@nekofar.com>
2024-09-18 18:30:32 +0400

    fix(env): default `APP_HOST` to `CF_PAGES_URL`
    ensure `APP_HOST` has a fallback value in case it's undefined.

--- apps/nouns-camp/src/constants/env.js
+++ apps/nouns-camp/src/constants/env.js
@@ -1,5 +1,5 @@
 export const CHAIN_ID = Number(process.env.NEXT_PUBLIC_CHAIN_ID);
 export const CAMP_CLIENT_ID = Number(process.env.NEXT_PUBLIC_CAMP_CLIENT_ID);
-export const APP_HOST = process.env.APP_HOST;
+export const APP_HOST = process.env.APP_HOST || process.env.CF_PAGES_URL;
 export const APP_PRODUCTION_URL = process.env.APP_PRODUCTION_URL;
 export const GA_ID = `${process.env.NEXT_PUBLIC_GA_ID}`;
