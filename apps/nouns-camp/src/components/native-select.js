73406c9099bb247132f0257d5fd9b2e77a6b73b4
prego <pedropregueiro@gmail.com>
2024-10-04 11:09:37 +0200

    feat(camp): add prop navigation to the navbar using <select>
    Co-authored-by: Olle Werme <olle.werme@gmail.com>
    (cherry picked from commit 951072a2030eb2a658d72e45f6dd7b2199093db0)

--- apps/nouns-camp/src/components/native-select.js
+++ apps/nouns-camp/src/components/native-select.js
@@ -1,12 +1,19 @@
 const NativeSelect = ({
   value,
   options,
+  groupedOptions,
   renderSelectedOption,
   onChange,
   style,
+  selectProps,
   ...props
 }) => {
-  const selectedOption = options.find((o) => String(o.value) === String(value));
+  const selectedOption = groupedOptions
+    ? groupedOptions
+        .flatMap((group) => group.options)
+        .find((o) => String(o.value) === String(value))
+    : options?.find((o) => String(o.value) === String(value));
+
   return (
     <span
       style={{
@@ -21,12 +28,23 @@ const NativeSelect = ({
         value={value}
         onChange={onChange}
         style={{ position: "absolute", inset: 0, opacity: 0 }}
+        {...selectProps}
       >
-        {options.map((o) => (
-          <option key={o.value} value={o.value}>
-            {o.label}
-          </option>
-        ))}
+        {groupedOptions
+          ? groupedOptions.map((group) => (
+              <optgroup key={group.label} label={group.label}>
+                {group.options.map((o) => (
+                  <option key={o.value} value={o.value}>
+                    {o.label}
+                  </option>
+                ))}
+              </optgroup>
+            ))
+          : options.map((o) => (
+              <option key={o.value} value={o.value}>
+                {o.label}
+              </option>
+            ))}
       </select>
     </span>
   );
