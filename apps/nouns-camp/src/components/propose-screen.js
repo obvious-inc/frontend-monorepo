17b29d94f1ef1bd5c6eef4277af89ff4d6d3e613
Milad Nekofar <milad@nekofar.com>
2024-10-19 20:55:22 +0400

    fix(propose-screen): handle NaN values in top-up logic
    Prevent errors when `executorEthBalance` or `payerTopUpValueData` are NaN. This ensures the application does not break due to unexpected data types.

--- apps/nouns-camp/src/components/propose-screen.js
+++ apps/nouns-camp/src/components/propose-screen.js
@@ -100,13 +100,15 @@ const ProposeScreen = ({ draftId, startNavigationTransition }) => {
     }
   }, BigInt(0));
 
-  const treasuryData = useTreasuryData();
   const payerTopUpValueData = useTokenBuyerEthNeeded(usdcSumValue);
-
+  const treasuryData = useTreasuryData();
+  const executorEthBalance = Number(treasuryData?.balances.executor.eth);
   const payerTopUpValue =
-    Number(treasuryData?.balances.executor.eth) > payerTopUpValueData
-      ? payerTopUpValueData
-      : 0;
+    isNaN(executorEthBalance) || isNaN(payerTopUpValueData)
+      ? 0
+      : executorEthBalance > payerTopUpValueData
+        ? payerTopUpValueData
+        : 0;
 
   const submit = async () => {
     // const buildCandidateSlug = (title) => {
