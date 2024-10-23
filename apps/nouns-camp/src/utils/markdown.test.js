97d21f94e865ead2b00314e6ab136a22e7f75094
olli <1403501+werme@users.noreply.github.com>
2024-10-23 11:42:42 +0200

    fix(camp): correctly apply and remove markdown blockquotes
    We previously didn’t handle all line boundaries, e.g. U+2028, which prevented
    some reposts from matching. This unfortunately won’t fix already broken ones.
    
    We are in your debt Benjamin.
    
    (cherry picked from commit 79757d2fc20c2e3fbb255ff76a7b4bd765edc4b8)

---
+++ apps/nouns-camp/src/utils/markdown.test.js
@@ -0,0 +1,68 @@
+import { expect, test } from "vitest";
+import { createRepostExtractor } from "./votes-and-feedbacks.js";
+
+const sourceReasons = [
+  "foo",
+  "bar",
+  `baz
+
++1
+
+> foo`,
+  "foo\u2028bar",
+];
+
+test("repost extraction", () => {
+  const extract = createRepostExtractor(
+    sourceReasons.map((reason) => ({ reason })),
+  );
+
+  const tests = [
+    { reason: "+1\n\n> foo", expectedMatchIndecies: [0] },
+    {
+      reason: `comment
+
++1
+
+> bar
+
+comment
+
++1
+
+>baz
+
+comment`,
+      expectedMatchIndecies: [1, 2],
+    },
+    {
+      reason: `comment
+
++1
+
+> baz
+>
+> +1
+>
+> > foo`,
+      expectedMatchIndecies: [2],
+    },
+    { reason: "+1\n\n> foo\u2028bar", expectedMatchIndecies: [0] },
+    { reason: "+1\n\n> foo\u2028> bar", expectedMatchIndecies: [3] },
+  ];
+
+  for (const { reason, expectedMatchIndecies } of tests) {
+    const [matchingPosts] = extract(reason);
+    const matchingReasons = matchingPosts.map((p) => p.reason);
+    const expectedMatchingReasons = expectedMatchIndecies.map(
+      (index) => sourceReasons[index],
+    );
+    for (const expectedMatchingReason of expectedMatchingReasons) {
+      expect(matchingReasons).toContain(expectedMatchingReason);
+    }
+    for (const matchingReason of matchingReasons) {
+      expect(expectedMatchingReasons).toContain(matchingReason);
+    }
+    expect(matchingPosts.length).toBe(expectedMatchIndecies.length);
+  }
+});
