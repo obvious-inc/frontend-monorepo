9df74af490a33312a0aca484e0e2f519e6c71717
Milad Nekofar <milad@nekofar.com>
2024-10-23 20:56:46 +0400

    fix(store): enable `delegator` field in queries
    corrected missing `delegator` fields to fix query functionality.
    This ensures accurate data retrieval for delegate changes.
    
    (cherry picked from commit c3a7291f355c2e22747fc05d30ccbbfc0eba739b)

--- apps/nouns-camp/src/nouns-subgraph.js
+++ apps/nouns-camp/src/nouns-subgraph.js
@@ -124,7 +124,7 @@ export const DELEGATION_EVENT_FIELDS = `
     }
     newDelegate { id }
     previousDelegate { id }
-  # delegator { id }
+    delegator { id }
     blockNumber
     blockTimestamp
   }`;
@@ -409,7 +409,7 @@ const parseDelegationEvent = (e) => {
   const parsedData = {
     ...e,
     blockTimestamp: parseTimestamp(e.blockTimestamp),
-    delegatorId: e.noun?.owner?.id,
+    delegatorId: e.delegator?.id,
     newAccountId: e.newDelegate?.id,
     previousAccountId: e.previousDelegate?.id,
     nounId: e.noun?.id,
