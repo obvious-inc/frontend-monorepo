5d07d60faeb8e6b8e28c9f0f3224bbb4a4ae87db
Milad Nekofar <milad@nekofar.com>
2024-10-26 12:13:57 +0400

    fix(store): handle undefined feedbacks and candidates
    Resolve potential crashes when candidate feedbacks or proposals are undefined by adding nullish coalescing checks.

--- apps/nouns-camp/src/store.js
+++ apps/nouns-camp/src/store.js
@@ -1541,7 +1541,7 @@ const createStore = ({ initialState, publicClient }) =>
         fetchCandidatesFeedbackPosts(proposalCandidates?.map((c) => c.id) ?? []);
         // Fetch Candidates the voter has commented on
         fetchProposalCandidates(
-          arrayUtils.unique(candidateFeedbacks.map((p) => p.candidateId)),
+          arrayUtils.unique(candidateFeedbacks?.map((p) => p.candidateId) ?? []),
         );
         // Fetch relevant noun data
         fetchNounsByIds(
@@ -1557,10 +1557,10 @@ const createStore = ({ initialState, publicClient }) =>
         fetchProposals(
           arrayUtils.unique([
             ...votes.map((p) => p.proposalId),
-            ...proposalFeedbacks.map((p) => p.proposalId),
-            ...sponsoredProposalCandidates.map(
+            ...proposalFeedbacks?.map((p) => p.proposalId) ?? [],
+            ...sponsoredProposalCandidates?.map(
               (c) => c.latestVersion?.proposalId,
-            ),
+            ) ?? [],
             ...propdates.map((p) => p.proposalId),
           ]),
         );
